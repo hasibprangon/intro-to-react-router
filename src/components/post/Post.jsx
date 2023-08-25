8a8bfdae2884f36ae758e5e14874839591c721b8
hasibprangon <hasibulhossainp09@gmail.com>
2023-08-25 20:05:15 +0600

    loading data using button and useNavigate

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
     const {id, title, body} = post;
+
+    const navigate = useNavigate();
+
+    const handleNavigation = () =>{
+        navigate(`/post/${id}`)
+    }
     return (
         <div className='bg-cyan-500 rounded-lg p-1 shadow-2xl flex flex-col justify-end'>
             <h1 className='font-bold text-2xl mt-4'>ID: {id}</h1>
             <h2 className='font-bold mt-2'>title:{title}</h2>
-            <Link className=' text-white mt-auto' to={`/post/${id}`}> Show Detail</Link>
-            <button className='rounded-full bg-cyan-600 p-2 hover:bg-cyan-700 mt-auto'>Show Post detail</button>
+
+
+            <Link className=' text-white mt-auto hover:bg-cyan-700 rounded-md' to={`/post/${id}`}> Show Detail</Link>
+            
+
+            <Link to={`/post/${id}`}><button className='rounded-full bg-cyan-600 p-2 hover:bg-cyan-700 mt-auto'>Show Post detail</button></Link>
+
+
+            <button className='rounded-full bg-cyan-600 p-2 hover:bg-cyan-700 mt-auto' onClick={handleNavigation}>Button with handler</button>
         </div>
     );
 };
--- src/components/postDetail/PostDetail.jsx
+++ src/components/postDetail/PostDetail.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetail = () => {
     const post = useLoaderData();
     // console.log(post);
     const {id, userId, title, body} = post;
+
+    const navigate = useNavigate();
+
+    const backNavigation = () =>{
+        navigate(-1);
+    }
     return (
         <div>
             <h3>Details about your post of:{id}</h3>
             <h2>{title}</h2>
             <p><small>{body}</small></p>
+            <button className='rounded-lg bg-slate-500 hover:bg-slate-600 p-3' onClick={backNavigation}>Go Back</button>
         </div>
     );
 };
